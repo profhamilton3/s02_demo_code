5b2630b2b24468fe11465286e91ae899ab78c2a5
T. Hamilton <profhamilton3@gmail.com>
2025-01-16 19:52:34 -0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+1.  Build a test system
+2.  Add Comment Block 
+3.  Add Wukong extention 
+4.  Save code with GitHub
+5.  Create loop to check button pressed 
+6.  Run Motor on button pressed
+7.  Save code with GitHub
+8.  Open saved code from repository
+'''
+
+#1. simple machine with microbit, wukong, red-motor and wheel
